4f88ff90f229d0cbaad6ac30d622c21314a379b1
Vinay Kumar <vinay@codecrux.com>
2018-01-19 16:26:52 +0530

    corrected error message when mail is not delivered

--- js/contact_me.js
+++ js/contact_me.js
@@ -49,7 +49,7 @@ $(function() {
                 $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                     .append("</button>");
                 $('#success > .alert-success')
-                    .append("<strong>Your message has been sent. </strong>");
+                    .append("<strong>We are sorry, something went wrong. Please try again. </strong>");
                 $('#success > .alert-success')
                     .append('</div>');
 
